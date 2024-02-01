57ad132218295a5a080f6171a0532eaddfa70dea
priyanshu-inu <priyanshuinuu@gmail.com>
2024-02-01 22:26:20 +0530

    add-folder

---
+++ assets/app.js
@@ -0,0 +1,7 @@
+let value = "";
+let btns = document.querySelectorAll(".btn");
+Array.from(btns).forEach((button))=>{
+    button.addEventListener('click' ,(e)=>{
+        if(.target.inner)
+    }
+}
